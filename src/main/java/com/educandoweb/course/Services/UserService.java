2247e409c423014ef5cea3a2d196b829fbcf9653
unknown <gustavobatista18@hotmail.com>
2025-01-17 23:42:11 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/Services/UserService.java
+++ src/main/java/com/educandoweb/course/Services/UserService.java
@@ -13,6 +13,8 @@ import com.educandoweb.course.Services.exceptions.ResourceNotFoundException;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
     @Autowired
@@ -40,11 +42,14 @@ public class UserService {
     }
 
     public User update(Long id,User obj){
+        try{
         User entity = repository.getReferenceById(id);
         updateData(entity,obj);
                 return repository.save(entity);
+        }catch(EntityNotFoundException e){
+                throw new ResourceNotFoundException(id);
             }
-        
+    }
             private void updateData(User entity, User obj) {
                 entity.setName(obj.getName());
                 entity.setEmail(obj.getEmail());
